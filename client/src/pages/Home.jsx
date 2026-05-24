8710169276fe81c4060c0a10bcb2efdbc51fd602
Chotu Ram <choturam21642@gmail.com>
2026-05-24 10:28:18 +0530

    make some changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
-import Steps from '../components/Steps'
+import Steps from '../components/Steps.jsx'
 import Description from '../components/Description.jsx'
 import Testimonial from '../components/Testimonial.jsx'
 import GenerateBtn from '../components/GenerateBtn.jsx'
